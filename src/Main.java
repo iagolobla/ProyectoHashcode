7bbce58211ea7c2cff5c53519f8dee054ea5f509
JoaquinTrinanes <quin.trinanes@gmail.com>
2018-03-01 18:57:17 +0100

    Clase Viaje creada

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,33 @@ import java.io.IOException;
 
 public class Main {
 
+    static int R, C, F, N, B, T;
+
     public static void main (String[] args) throws Exception {
+
+        String in = "a_example.in";
+
+        BufferedReader input = leerArchivo(in);
+
+        String primeraLinea = input.readLine();
+        String[] tokens = primeraLinea.split("\\s");
+
+        R = Integer.parseInt(tokens[0]);
+        C = Integer.parseInt(tokens[1]);
+        F = Integer.parseInt(tokens[2]);
+        N = Integer.parseInt(tokens[3]);
+        B = Integer.parseInt(tokens[4]);
+        T = Integer.parseInt(tokens[5]);
+
+
+
+
+
+
 
     }
 
-    BufferedReader leerArchivo(String nombre) throws IOException {
+    private static BufferedReader leerArchivo(String nombre) throws IOException {
         return new BufferedReader(new FileReader(nombre));
     }
 
---
+++ src/Viaje.java
@@ -0,0 +1,38 @@
+public class Viaje {
+
+    private Celda celdaInicio, celdaFin;
+    private int s;
+    private int f;
+
+    public Celda getCeldaInicio() {
+        return celdaInicio;
+    }
+
+    public void setCeldaInicio(Celda celdaInicio) {
+        this.celdaInicio = celdaInicio;
+    }
+
+    public Celda getCeldaFin() {
+        return celdaFin;
+    }
+
+    public void setCeldaFin(Celda celdaFin) {
+        this.celdaFin = celdaFin;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+}

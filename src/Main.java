33abe3944b120dd052aa3140c11994e35b0c6ace
JoaquinTrinanes <quin.trinanes@gmail.com>
2018-03-01 18:45:07 +0100

    Añadidas excepciones y lector de archivo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main (String[] args) {
-        
+    public static void main (String[] args) throws Exception {
+
+    }
+
+    BufferedReader leerArchivo(String nombre) throws IOException {
+        return new BufferedReader(new FileReader(nombre));
     }
 
 }
